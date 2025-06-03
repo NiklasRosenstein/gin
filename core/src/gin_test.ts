8238114b98229cd1880a2e80baab79904543055d
Niklas Rosenstein <rosensteinniklas@gmail.com>
2025-06-03 03:40:45 +0200

    fix test

--- core/src/gin_test.ts
+++ core/src/gin_test.ts
@@ -3,5 +3,5 @@ import { Gin } from "@gin/core";
 
 Deno.test(function testGinDefaultPackageMapping() {
   const gin = new Gin();
-  assertEquals(gin.resolvePackageNameFromApiVersion("webapp.gin.jsr.io/v1alpha1"), "@gin/webapp-v1alpha1");
+  assertEquals(gin.resolvePackageNameFromApiVersion("webapp.gin.jsr.io/v1alpha1"), "jsr:@gin/webapp-v1alpha1");
 });
