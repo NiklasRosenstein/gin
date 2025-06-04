7fc3246368d84e987482536052e5c877cde3fc38
Niklas Rosenstein <rosensteinniklas@gmail.com>
2025-06-04 19:04:56 +0200

    start adding sops implementation

---
+++ packages/sops/README.md
@@ -0,0 +1,23 @@
+# @gin/sops
+
+This package implements the `SecretProvider` API from the `@gin/core` package for a SOPS file. All values are returned
+as `SecretValue` objects from the `@gin/core` package. This package wraps the `sops` command-line tool, so you need to
+have it installed your system.
+
+## Usage
+
+```ts
+import { Sops } from "@gin/sops";
+const sops = new Sops({ path: `${import.meta.dirname}/sops.yaml` });
+const secret = sops.get("dotted.path.to.secret");
+```
+
+Your environment must be set up for the `sops` command to work and be able to decrypt the SOPS file. This typically
+means setting up the necessary keys in your environment, such as `SOPS_AGE_KEY`. See https://getsops.io/ for more
+information.
+
+## Deno Permissions
+
+| Permission         | Rationale                                                                        |
+| ------------------ | -------------------------------------------------------------------------------- |
+| `--allow-run=sops` | This package uses the `sops` command-line tool to read secrets from a SOPS file. |
---
+++ packages/sops/mod_test.ts
@@ -0,0 +1,53 @@
+import { assertEquals } from "@std/assert/equals";
+import { Sops } from "./mod.ts";
+import { SecretValue } from "../core/src/mod.ts";
+import { assertRejects } from "@std/assert/rejects";
+
+Deno.test(async function testJsonPathInSopsGet() {
+  const sops = new Sops({
+    path: "<memory>",
+    data: {
+      stringValue: "test",
+      numberValue: 42,
+      objectValue: {
+        nestedString: "nested",
+        nestedNumber: 100,
+      },
+      arrayValue: [1, 2, 3],
+      nestedArrayValue: [
+        { key: "value1" },
+        { key: "value2" },
+      ],
+    },
+  });
+
+  assertEquals(await sops.getString("stringValue"), SecretValue.of("test"));
+  assertRejects(
+    () => sops.getString("numberValue"),
+    'Failed to resolve "string" secret "numberValue", expected "string" got "number".',
+  );
+
+  assertEquals(await sops.getArray("arrayValue"), SecretValue.of([1, 2, 3]));
+  assertRejects(
+    () => sops.getArray("stringValue"),
+    'Failed to resolve "array" secret "stringValue", expected "array" got "string".',
+  );
+  assertRejects(
+    () => sops.getArray("objectValue"),
+    'Failed to resolve "array" secret "objectValue", expected "array" got "object".',
+  );
+
+  assertEquals(
+    await sops.getObject("objectValue"),
+    SecretValue.of({
+      nestedString: "nested",
+      nestedNumber: 100,
+    }),
+  );
+  assertRejects(
+    () => sops.getObject("arrayValue"),
+    'Failed to resolve "object" secret "objectValue", expected "object" got "array".',
+  );
+
+  assertEquals(await sops.getArray("nestedArrayValue[*].key"), SecretValue.of(["value1", "value2"]));
+});
