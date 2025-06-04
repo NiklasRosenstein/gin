4263ec1f58d851fb83c68d63f41e681383c8f3bf
Niklas Rosenstein <rosensteinniklas@gmail.com>
2025-06-04 23:24:49 +0200

    fix running test-all.ts from picky-publisher.ts

--- scripts/picky-publisher.ts
+++ scripts/picky-publisher.ts
@@ -3,24 +3,24 @@
  * If the `{version}` does not match the version defined in the `deno.json`, it will throw an error.
  */
 
-let [packageName, version] = Deno.args[0]!.split("@v");
-if (!packageName || !version) {
+const [pkg, version] = Deno.args[0]!.split("@v");
+if (!pkg || !version) {
   console.error("Usage: deno run scripts/picky-publisher.ts <packageName>@v<version>");
   Deno.exit(1);
 }
 
-packageName = `packages/${packageName}`;
+const pkgPath = `packages/${pkg}`;
 
-const denoJson = JSON.parse(Deno.readTextFileSync(`${packageName}/deno.json`));
+const denoJson = JSON.parse(Deno.readTextFileSync(`${pkgPath}/deno.json`));
 if (denoJson.version !== version) {
   console.error(`Version mismatch: expected ${denoJson.version}, but got ${version}`);
   Deno.exit(1);
 }
 
-console.log(`Linting package '${packageName}' ...`);
+console.log(`Linting package '${pkgPath}' ...`);
 const lintResult = await new Deno.Command(Deno.execPath(), {
   args: ["lint"],
-  cwd: packageName,
+  cwd: pkgPath,
 }).spawn().output();
 
 if (lintResult.code !== 0) {
@@ -28,9 +28,9 @@ if (lintResult.code !== 0) {
   Deno.exit(1);
 }
 
-console.log(`Testing package '${packageName}' ...`);
+console.log(`Testing package '${pkgPath}' ...`);
 const testResult = await new Deno.Command(Deno.execPath(), {
-  args: ["run", "./scripts/test-all.ts", packageName],
+  args: ["run", "--allow-all", "./scripts/test-all.ts", pkg],
 }).spawn().output();
 
 if (testResult.code !== 0) {
@@ -38,10 +38,10 @@ if (testResult.code !== 0) {
   Deno.exit(1);
 }
 
-console.log(`Publishing package '${packageName}' ...`);
+console.log(`Publishing package '${pkgPath}' ...`);
 const publishResult = await new Deno.Command(Deno.execPath(), {
   args: ["publish"],
-  cwd: packageName,
+  cwd: pkgPath,
 }).spawn().output();
 
 if (publishResult.code !== 0) {
