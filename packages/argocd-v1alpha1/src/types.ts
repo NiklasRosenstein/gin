4794bbe71f4e37b0a025d0608c595bb5ed21e91c
Niklas Rosenstein <rosensteinniklas@gmail.com>
2025-06-06 02:36:46 +0200

    argocd-v1alpha1: Add new `@gin/argocd-v1alpha1/types` module

---
+++ packages/argocd-v1alpha1/src/types.ts
@@ -0,0 +1,118 @@
+/**
+ * ArgoCD-specific types.
+ */
+
+import type { KubernetesObject, ObjectMeta, SecretValue } from "@gin/core";
+
+/**
+ * See
+ * - https://argo-cd.readthedocs.io/en/stable/operator-manual/argocd-repo-creds-yaml/
+ * - https://argo-cd.readthedocs.io/en/stable/user-guide/private-repositories/
+ */
+export interface RepositoryCredentialTemplate extends KubernetesObject {
+  apiVersion: "v1";
+  kind: "Secret";
+  metadata: ObjectMeta & {
+    labels: {
+      "argocd.argoproj.io/secret-type": "repo-creds";
+    };
+  };
+  data?: {
+    "project"?: string;
+    "name"?: string;
+    "type": "git" | "helm";
+    "url": string;
+    "username"?: string;
+    "password"?: SecretValue;
+    "sshPrivateKey"?: SecretValue;
+    "githubAppID"?: string;
+    "githubAppInstallationID"?: string;
+    "githubAppPrivateKey"?: SecretValue;
+    "githubAppEnterpriseBaseUrl "?: string;
+  };
+  stringData?: RepositoryCredentialTemplate["data"];
+}
+
+/**
+ * See
+ * - https://argo-cd.readthedocs.io/en/stable/operator-manual/argocd-repositories-yaml/
+ * - https://argo-cd.readthedocs.io/en/stable/user-guide/private-repositories/
+ */
+export interface Repository extends KubernetesObject {
+  apiVersion: "v1";
+  kind: "Secret";
+  metadata: ObjectMeta & {
+    labels: {
+      "argocd.argoproj.io/secret-type": "repository";
+    };
+  };
+  data?: {
+    "project"?: string;
+    "name"?: string;
+    "type"?: "git" | "helm";
+    "url": string;
+    "username"?: string;
+    "password"?: SecretValue;
+    "bearerToken"?: SecretValue;
+    "sshPrivateKey"?: SecretValue;
+    "insecure"?: "true" | "false";
+    "forceHttpBasicAuth"?: "true" | "false";
+    "enableLfs"?: "true" | "false";
+    "enableOCI"?: "true" | "false";
+    "useAzureWorkloadIdentity"?: "true" | "false";
+  };
+  stringData?: Repository["data"];
+}
+
+/**
+ * See
+ * - https://argo-cd.readthedocs.io/en/stable/operator-manual/declarative-setup/#clusters
+ */
+export interface Cluster extends KubernetesObject {
+  apiVersion: "v1";
+  kind: "Secret";
+  metadata: ObjectMeta & {
+    labels: {
+      "argocd.argoproj.io/secret-type": "cluster";
+    };
+  };
+  data?: {
+    "name"?: string;
+    "server": string;
+    "config"?: SecretValue;
+  };
+  stringData: Cluster["data"];
+}
+
+/**
+ * See
+ * - https://argo-cd.readthedocs.io/en/stable/operator-manual/declarative-setup/#clusters
+ */
+export interface ClusterSecret {
+  username?: string;
+  password?: string;
+  bearerToken?: string;
+  awsAuthConfig?: {
+    clusterName: string;
+    roleARN: string;
+    profile?: string;
+  };
+  execProviderConfig?: {
+    command: string;
+    args?: string[];
+    env?: Record<string, string>;
+    apiVersion?: string;
+    installHint?: string;
+  };
+  proxyUrl?: string;
+  tlsClientConfig?: {
+    caData?: string;
+    certData?: string;
+    insecure?: boolean;
+    keyData?: string;
+    serverName?: string;
+  };
+  disableCompression?: boolean;
+}
+
+// TODO: Add definitions for Application, ApplicationSet and Project resources
